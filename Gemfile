bbf3d9710848ba043f3407d9734c7bc1c0ae47d5
junhan605 <junhan605>
2017-06-09 04:32:54 +0200

    add approve function on user creation by admin

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'activeadmin', '~> 1.0.0.pre4'
 
 group :development, :test do
   gem 'byebug', platform: :mri
+  gem 'binding_of_caller'
 end
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -12,29 +12,95 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
-  permit_params :email, :name, :password, :password_confirmation, :suspend, :companies
+  permit_params :email, :name, :password, :password_confirmation, :suspend, :approved, companies: [], company_ids: []
 
   index do
     column :name
     column :email
-    column :companies
-    column do
-        link_to 'Suspend', "#"
-    end
-    actions
 
+    actions name: "Approve", defaults: false do |resource|
+      if resource.approved?
+        "Approved"
+      else
+        link_to("Approve", approve_admin_user_path(resource), method: :post
+        )
+      end
+    end
+
+    # column :approved do |resource|
+    #   if resource.approved == false
+    #     link_to("Approve",
+    #       approve_admin_user_path(resource),
+    #       remote: true, method: :post
+    #     )
+    #   else
+    #     resource.approved
+    #   end
+    # end
+
+    actions name: "Suspend", defaults: false do |resource|
+      params = { user_id: resource.id }
+      link_to(resource.suspend ? "Active" : "Suspend",
+        suspend_admin_user_path(resource, params), method: :post)
+    end
+
+    actions
   end
+
+  # action_item :approve, only:[:index] do
+  #   link_to("Approve",
+  #     approve_admin_user_path(resource),
+  #     remote: true, method: :post
+  #   )
+  # end
+
+  # action_item :suspend, only:[:index]
+  #   link_to("Suspend",
+  #     suspend_admin_user_path(resource) if resource?,
+  #     remote: true, method: :post
+  #   )
+  # end
 
   form do |f|
     f.inputs 'User' do
       f.input :name
       f.input :email
-      f.input :password
-      f.input :password_confirmation
+      # f.input :password
+      # f.input :password_confirmation
+      f.input :approved
       f.input :suspend
       f.input :companies, :as => :check_boxes
     end
+
     f.actions
   end
 
+  member_action :approve, method: :post, only: :index do
+  end
+
+  member_action :suspend, method: :post, only: :index do
+  end
+
+  controller do
+    def suspend
+      resource.update_attributes! suspend: resource.suspend ? false : true
+      redirect_to admin_users_path
+    end
+
+    def approve
+      resource.update_attributes! approved: true
+      redirect_to admin_users_path
+    end
+  end
+
+  before_create do |user|
+    user.password = user.generated_password
+    user.password_confirmation = user.password
+  end
+
+  after_create do |user|
+    # AdminMailer.new_user_waiting_for_approval(self).deliver
+    # mail(to: user.email, subject: "Account was created for you", contents: user.password)
+    # RegistrationMailer.welcome(user, user.password).deliver
+  end
 end
